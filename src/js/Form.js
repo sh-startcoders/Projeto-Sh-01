2f587b0261743fa68bc45298c05070cc70dc95fd
dandankara <cunhacb22@gmail.com>
2021-09-22 17:19:41 -0300

    Add logo, estilização geral nas fonts e nome das functions

--- src/js/Form.js
+++ src/js/Form.js
@@ -2,22 +2,44 @@ function ShowInputSexOther(el) {
 
   let InputSexOther = document.getElementById('outros')
 
-  if (el.value === 'othersex') {
-    InputSexOther.style.display = 'block';
-  } else {
-    InputSexOther.style.display = 'none';
+  if (el.value === 'othersex' ? InputSexOther.style.display = 'block' : InputSexOther.style.display = 'none') {
   }
 }
 
-function ShowInputOtherDocuments(el){
+function ShowInputOtherDocuments(el) {
   let InputOtherDocuments = document.getElementById('InputOutroDocumento');
 
-  if(el.value == 'otherdocuments'){
-    InputOtherDocuments.style.display = 'block';
-  }else{
-    InputOtherDocuments.style.display = 'none'
+  if (el.value ? InputOtherDocuments.style.display = 'block' : InputOtherDocuments = 'none') { 
   }
 }
 
+function medicalTreatment(el) {
+  let inputMedical = document.getElementById('inputMedical');
 
-// Fazer as mascaras de CPF RG E CPF 
+  if (el.value == "Sim" ? inputMedical.style.display = "block" : inputMedical.style.display = "none");
+}
+
+function medications(el) {
+  let inputMedications = document.getElementById('inputMedications');
+
+  if (el.value == "Sim" ? inputMedications.style.display = "block" : inputMedications.style.display = "none");
+}
+
+function pcd(el) {
+  let inputPcd = document.getElementById('inputPcd');
+
+  if (el.value == "Sim" ? inputPcd.style.display = "block" : inputPcd.style.display = "none");
+}
+
+function illness(el) {
+  let inputIllness = document.getElementById('inputIllness');
+
+  if (el.value == "Sim" ? inputIllness.style.display = "block" : inputIllness.style.display = "none");
+}
+
+function streetIllness(el){
+  let inputStreetIllness = document.getElementById("inputStreetIlness");
+
+  if( el.value == 'Sim' ? inputStreetIllness.style.display = "block" : inputStreetIllness.style.display = "none"){
+  }
+} 
