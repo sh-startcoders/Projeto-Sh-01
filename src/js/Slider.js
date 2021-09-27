0935c0ba01264c737d7142cc6141650b9e32bfb3
dandankara <cunhacb22@gmail.com>
2021-09-27 14:45:28 -0300

    Adição do slider ranged e formatção do HTML

--- src/js/Slider.js
+++ src/js/Slider.js
@@ -1,13 +1,10 @@
 const slideValue = document.getElementById('SliderValeuSpan');
 const inputSlider = document.getElementById('InputRange');
 
-// Oninput é usado quando for trabalhar 
-// com inputs, quase igual o onchange
+// Oninput é usado quando for trabalhar com inputs, quase igual o onchange
 inputSlider.oninput = (() => {
   let value = inputSlider.value; //Pega o valor que está no slider
   slideValue.textContent = value; //Pega o conteúdo do valor que tá no input
-  slideValue.style.left(value / 2) + '%' //move a div de acordo que o scroll move 
-  slideValue.classList.add("show") //mostra o grau de mobilidade na div que aparece em cima do slider
 });
 
 inputSlider.onblur = (() => {
