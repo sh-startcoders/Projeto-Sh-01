8e611642cef3362f1fb8bc133b3425a84ddf8638
dandankara <cunhacb22@gmail.com>
2021-09-28 17:21:23 -0300

    Adição da Terceira parte do formulário juntamente com as funções dos inputs

--- src/js/Form.js
+++ src/js/Form.js
@@ -1,3 +1,6 @@
+// Esse script é somente as perguntas que precisam que apareça um input para text
+
+// Função para mostrar o input de sexo == 'outros'
 function ShowInputSexOther(el) {
 
   let InputSexOther = document.getElementById('outros')
@@ -6,6 +9,7 @@ function ShowInputSexOther(el) {
   }
 }
 
+// Função para mostrar os inputs para digitar os documentos
 function ShowInputOtherDocuments(el) {
   let InputOtherDocuments = document.getElementById('InputOutroDocumento');
 
@@ -13,24 +17,29 @@ function ShowInputOtherDocuments(el) {
   }
 }
 
+// Função para mostrar o input de 'acompanhamento médico'
 function medicalTreatment(el) {
   let inputMedical = document.getElementById('inputMedical');
 
   if (el.value == "Sim" ? inputMedical.style.display = "block" : inputMedical.style.display = "none");
 }
 
+
+// Função para mostrar o input caso o usuário tome alguma medicação
 function medications(el) {
   let inputMedications = document.getElementById('inputMedications');
 
   if (el.value == "Sim" ? inputMedications.style.display = "block" : inputMedications.style.display = "none");
 }
 
+// Função para mostrar o input caso o usuário tenha alguma deficiência
 function pcd(el) {
   let inputPcd = document.getElementById('inputPcd');
 
   if (el.value == "Sim" ? inputPcd.style.display = "block" : inputPcd.style.display = "none");
 }
 
+// Função mostrando o input caso o usuário tenha se contaminado na rua
 function illness(el) {
   let inputIllness = document.getElementById('inputIllness');
 
@@ -41,5 +50,33 @@ function streetIllness(el) {
   let inputStreetIllness = document.getElementById("inputStreetIlness");
 
   if (el.value == 'Sim' ? inputStreetIllness.style.display = "block" : inputStreetIllness.style.display = "none") {
+  }
+}
+
+// Função para mostrar o input de pq está sozinho
+function ReasonAlone(el) {
+  let InputReasonAlone = document.getElementById('ShowDivInput');
+
+  if (el.value == 'Sim' ? InputReasonAlone.style.display = 'block' : InputReasonAlone.style.display = 'none') {
+
+  }
+}
+
+// Função para mostrar o input perguntando qual a relação com o pessoa
+function ShowRelations(el) {
+  let InputOtherRelation  = document.getElementById('InputOtherRelation');
+
+  if (el.value == 'outrarelacao' ? InputOtherRelation.style.display = 'block' : InputOtherRelation.style.display = 'none') {
+
+  }
+}
+
+
+// Função mostrando input se a familia sabe da situação de rua
+function ShowInputFamily(el) {
+  let InputFamily = document.getElementById('DivFamilia');
+
+  if(el.value == 'Sim' ? InputFamily.style.display = 'block' : InputFamily.style.display = 'none'){
+
   }
 }
